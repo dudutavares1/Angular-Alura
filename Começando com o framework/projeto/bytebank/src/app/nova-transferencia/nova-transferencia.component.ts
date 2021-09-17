3395a03719d5cd064efc365fda055dde815af6ec
Dudu Tavares <skynetdudu@icloud.com>
2021-09-17 00:00:54 -0300

    fim da aula 1 de angular

---
+++ Começando com o framework/projeto/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.css']
+})
+export class NovaTransferenciaComponent implements OnInit {
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+  console.log('solicitada nova transferencia')
+}
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
